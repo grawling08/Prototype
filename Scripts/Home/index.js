49e66acf12f10c57022dd63d9ad176b1093fd8d3
Grawling <grawling08@gmail.com>
2021-05-11 09:14:06 +0800

    Add project files.

---
+++ Scripts/Home/index.js
@@ -0,0 +1,11 @@
+﻿import Vue from 'vue';
+import HomeComponent from './home.vue';
+import AboutComponent from './aboutus.vue';
+
+new Vue({
+    el: "#app",
+    components: {
+        HomeComponent,
+        AboutComponent
+    }
+});
